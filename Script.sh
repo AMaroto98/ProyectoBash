a57f6f972495d818edb10efd65470c69959077bb
Antonio Maroto <antoniomarotoblasco@gmail.com>
2023-02-06 19:51:53 +0100

    Añadida funcionalidad en 9)

--- Script.sh
+++ Script.sh
@@ -92,16 +92,16 @@ case $mainOption in
 					;;
 
 				# Firefox
-				2) 	echo "Ahora empezará la descarga de Mozilla Firefox"
+				2) 	echo -e "\033[1;32mAhora empezará la descarga de Mozilla Firefox\033[0m"
 					
 					sudo snap install firefox
 
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 
 					sleep 2 
 
-					echo -e "Comprobando la versión de Mozilla Firefox"
+					echo -e "\033[1;32mComprobando la versión de Mozilla Firefox\033[0m"
 					sleep 2
 					firefox --version
 					
@@ -109,15 +109,15 @@ case $mainOption in
 					;;
 
 				# Chromium
-				3) 	echo "Ahora empezará la descarga de Chromium"
+				3) 	echo -e "\033[1;32mAhora empezará la descarga de Chromium\033[0m"
 					
 					sudo snap install chromium
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 
 					sleep 4 
 
-					echo -e "Comprobando la versión de Chromium"
+					echo -e "\033[1;32mComprobando la versión de Chromium\033[0m"
 					sleep 2
 					chromium --version
 					
@@ -125,15 +125,15 @@ case $mainOption in
 					;;
 
 				# Brave
-				4) 	echo "Ahora empezará la descarga de Brave"
+				4) 	echo -e "\033[1;32mAhora empezará la descarga de Brave\033[0m"
 					
 					sudo snap install brave
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 
 					sleep 4 
 
-					echo -e "Comprobando la versión de Brave"
+					echo -e "\033[1;32mComprobando la versión de Brave\033[0m"
 					sleep 2
 					brave --version
 					
@@ -182,25 +182,25 @@ case $mainOption in
 	    	case $nestedOption in
 
 	        	# Spotify
-		        1) 	echo "Ahora empezará la descarga de Spotify"
+		        1) 	echo -e "\033[1;32mAhora empezará la descarga de Spotify\033[0m"
 
 		            sudo snap install spotify
 		            sleep 2
-		            echo -e "Instalación completada"
+		            echo -e "\033[1;32mInstalación completada\033[0m"
 		            
 		            sleep 4 
 		            ;;
 
 	        	# VLC
-		        2) 	echo "Ahora empezará la descarga de VLC"
+		        2) 	echo -e "\033[1;32mAhora empezará la descarga de VLC\033[0m"
 
 		            sudo snap install vlc
 		            sleep 2
-		            echo -e "Instalación completada"
+		            echo -e "\033[1;32mInstalación completada\033[0m"
 
 		            sleep 4 
 
-		            echo -e "Comprobando la versión de VLC"
+		            echo -e "\033[1;32mComprobando la versión de VLC\033[0m"
 		            sleep 2
 		            vlc --version
 		            
@@ -208,15 +208,15 @@ case $mainOption in
 		            ;;
 
 		        # Audacity
-		        3) 	echo "Ahora empezará la descarga de Audacity"
+		        3) 	echo -e "\033[1;32mAhora empezará la descarga de Audacity\033[0m"
 		            
 		            sudo apt install audacity -y
 		            sleep 2
-		            echo -e "Instalación completada"
+		            echo -e "\033[1;32mInstalación completada\033[0m"
 
 		            sleep 4 
 
-		            echo -e "Comprobando la versión de Audacity"
+		            echo -e "\033[1;32mComprobando la versión de Audacity\033[0m"
 		            sleep 2
 		            audacity --version
 		            
@@ -224,15 +224,15 @@ case $mainOption in
 		            ;;
 
 		        # KDENLIVE
-		        4) 	echo "Ahora empezará la descarga de KdenLive"
+		        4) 	echo -e "\033[1;32mAhora empezará la descarga de KdenLive\033[0m"
 		            
 		            sudo snap install kdenlive
 		            sleep 2
-		            echo -e "Instalación completada"
+		            echo -e "\033[1;32mInstalación completada\033[0m"
 
 		            sleep 4 
 
-		            echo -e "Comprobando la versión de KdenLive"
+		            echo -e "\033[1;32mComprobando la versión de KdenLive\033[0m"
 		            sleep 2
 		            kdenlive --version
 		            
@@ -240,7 +240,7 @@ case $mainOption in
 		            ;;
 
 		        # Exit
-				5) echo "Volviendo al menú principal"
+				5) echo -e "\033[1;32mVolviendo al menú principal\033[0m"
 					
 					sleep 2
 					break
@@ -271,7 +271,7 @@ case $mainOption in
 			echo -e "\033[1;36m6) Exit\033[0m"
 			echo -e "\033[1;36m========================\033[0m"
 
-			read -p "Selecciona una opción: " nestedOption
+			read -p $'\033[1;32mSelecciona una opción:\033[0m ' nestedOption
 
 			if ! [[ $nestedOption =~ $validation ]]; then
 				echo -e "\033[1;31mPor favor, introduce un número.\033[0m"
@@ -282,15 +282,15 @@ case $mainOption in
 			case $nestedOption in
 
 				# VS Code
-				1) 	echo "Ahora empezará la descarga de VS Code"
+				1) 	echo -e "\033[1;32mAhora empezará la descarga de VS Code\033[0m"
 					
 					sudo snap install --classic code 
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 
 					sleep 4 
 
-					echo -e "Comprobando la versión de VS Code"
+					echo -e "\033[1;32mComprobando la versión de VS Code\033[0m"
 					sleep 2
 					code --version
 					
@@ -298,15 +298,15 @@ case $mainOption in
 					;;
 
 				# Eclipse
-				2) 	echo "Ahora empezará la descarga de Eclipse"
+				2) 	echo -e "\033[1;32mAhora empezará la descarga de Eclipse\033[0m"
 					
                     sudo snap install --classic eclipse 
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 
 					sleep 4 
 
-					echo -e "Comprobando la versión de Eclipse"
+					echo -e "\033[1;32mComprobando la versión de Eclipse\033[0m"
 					sleep 2
 					eclipse --version
 					
@@ -314,15 +314,15 @@ case $mainOption in
 					;;
 
                 # JDK
-				3) 	echo "Ahora empezará la descarga de JDK"
+				3) 	echo -e "\033[1;32mAhora empezará la descarga de JDK\033[0m"
 					
 					sudo apt install default-jdk -y 
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 
 					sleep 4 
 
-					echo -e "Comprobando la versión de JDK"
+					echo -e "\033[1;32mComprobando la versión de JDK\033[0m"
 					sleep 2
 					java --version
 					
@@ -330,15 +330,15 @@ case $mainOption in
 					;;
 
 				# Python
-				4) 	echo "Ahora empezará la descarga de Python"
+				4) 	echo -e "\033[1;32mAhora empezará la descarga de Python\033[0m"
 					
 					sudo apt install python3
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 
 					sleep 4 
 
-					echo -e "Comprobando la versión de Python"
+					echo -e "\033[1;32mComprobando la versión de Python\033[0m"
 					sleep 2
 					python3 --version
 					
@@ -346,15 +346,15 @@ case $mainOption in
 					;;
 
 				#MySQL
-				5) 	echo "Ahora empezará la descarga de MySQL"
+				5) 	echo -e "\033[1;32mAhora empezará la descarga de MySQL\033[0m"
 					
 					sudo apt install mysql-server -y
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 
 					sleep 4 
 
-					echo -e "Comprobando la versión de MySQL"
+					echo -e "\033[1;32mComprobando la versión de MySQL\033[0m"
 					sleep 2
 					mysql --version
 					
@@ -362,7 +362,7 @@ case $mainOption in
 					;;
 
 				# Exit
-				6) echo "Volviendo al menú principal"
+				6) echo -e "\033[1;32mVolviendo al menú principal\033[0m"
 					
 					sleep 2
 					break
@@ -389,7 +389,7 @@ case $mainOption in
 			echo -e "\033[1;36m2) Exit\033[0m"
 			echo -e "\033[1;36m========================\033[0m"
 
-			read -p "Selecciona una opción: " nestedOption
+			read -p $'\033[1;32mSelecciona una opción:\033[0m ' nestedOption
 
 			if ! [[ $nestedOption =~ $validation ]]; then
 				echo -e "\033[1;31mPor favor, introduce un número.\033[0m"
@@ -400,16 +400,16 @@ case $mainOption in
 			case $nestedOption in
 
 				# qBittorrent
-				1) 	echo "Ahora empezará la descarga de qBittorrent"
+				1) 	echo -e "\033[1;32mAhora empezará la descarga de qBittorrent\033[0m"
 					
 					sudo apt update
 					sudo apt install qbittorrent -y
 					
 					sleep 2
 
-					echo -e "Se ha completado la instalación de qBittorrent"
+					echo -e "\033[1;32mSe ha completado la instalación de qBittorrent\033[0m"
 
-					echo -e "Comprobando la versión de qBittorrent"
+					echo -e "\033[1;32mComprobando la versión de qBittorrent\033[0m"
 
 					qbittorrent --version
 					
@@ -417,7 +417,7 @@ case $mainOption in
 					;;
 
 				# Exit
-				2) echo "Volviendo al menú principal"
+				2) echo -e "\033[1;32mVolviendo al menú principal\033[0m"
 					
 					sleep 2
 					break
@@ -456,15 +456,15 @@ case $mainOption in
 			case $nestedOption in
 
 				# Slack
-				1) 	echo "Ahora empezará la descarga de Slack"
+				1) 	echo -e "Ahora empezará la descarga de Slack\033[0m"
 					
 					sudo snap install slack
 					
 					sleep 2
 
-					echo -e "Se ha completado la instalación de Slack"
+					echo -e "Se ha completado la instalación de Slack\033[0m"
 
-					echo -e "Comprobando la versión de Slack"
+					echo -e "Comprobando la versión de Slack\033[0m"
 
 					slack --version
 					
@@ -472,17 +472,17 @@ case $mainOption in
 					;;
 
 				# Telegram
-				2) 	echo "Ahora empezará la descarga de Telegram"
+				2) 	echo -e "\033[1;32mAhora empezará la descarga de Telegram\033[0m"
 					
 					sudo snap install telegram-desktop
 
 					sleep 2
 
-					echo -e "Se ha completado la instalación de Telegram"
+					echo -e "\033[1;32mSe ha completado la instalación de Telegram\033[0m"
 					;;
 
 				# Exit
-				3) echo "Volviendo al menú principal"
+				3) echo -e "\033[1;32mVolviendo al menú principal\033[0m"
 					
 					sleep 2
 					break
@@ -509,7 +509,7 @@ case $mainOption in
 			echo -e "\033[1;36m2) Exit\033[0m"
 			echo -e "\033[1;36m========================\033[0m"
 
-			read -p "Selecciona una opción: " nestedOption
+			read -p $'\033[1;32mSelecciona una opción:\033[0m ' nestedOption
 
 			if ! [[ $nestedOption =~ $validation ]]; then
 				echo -e "\033[1;31mPor favor, introduce un número.\033[0m"
@@ -564,7 +564,7 @@ case $mainOption in
 			echo -e "\033[1;36m2) Exit\033[0m"
 			echo -e "\033[1;36m========================\033[0m"
 
-			read -p "Selecciona una opción: " nestedOption
+			read -p $'\033[1;32mSelecciona una opción:\033[0m ' nestedOption
 
 			if ! [[ $nestedOption =~ $validation ]]; then
 				echo -e "\033[1;31mPor favor, introduce un número.\033[0m"
@@ -575,7 +575,7 @@ case $mainOption in
 			case $nestedOption in
 
 			    # LibreOffice
-		        1) 	echo "Ahora empezará la descarga de LibreOffice"
+		        1) 	echo -e "\033[1;32mAhora empezará la descarga de LibreOffice\033[0m"
 
 		            sudo add-apt-repository ppa:libreoffice/ppa  
 
@@ -583,17 +583,17 @@ case $mainOption in
 		            sudo apt install libreoffice -y
 
 		            sleep 2
-		            echo -e "Instalación completada"
+		            echo -e "\033[1;32mInstalación completada\033[0m"
 		            
 		            sleep 4 
 
-		            echo -e "Comprobando la versión de LibreOffice"
+		            echo -e "\033[1;32mComprobando la versión de LibreOffice\033[0m"
 		            sleep 2
 		            libreoffice --version
 		            ;;
 
 		        # Exit
-				2) echo "Volviendo al menú principal"
+				2) echo -e "\033[1;32mVolviendo al menú principal\033[0m"
 					
 					sleep 2
 					break
@@ -622,7 +622,7 @@ case $mainOption in
 			echo -e "\033[1;36m4) Exit\033[0m"
 			echo -e "\033[1;36m========================\033[0m"
 
-			read -p "Selecciona una opción: " nestedOption
+			read -p $'\033[1;32mSelecciona una opción:\033[0m ' nestedOption
 
 			if ! [[ $nestedOption =~ $validation ]]; then
 				echo -e "\033[1;31mPor favor, introduce un número.\033[0m"
@@ -633,27 +633,27 @@ case $mainOption in
 			case $nestedOption in
 
 				# Steam
-				1) 	echo "Ahora empezará la descarga de Steam"
+				1) 	echo -e "\033[1;32mAhora empezará la descarga de Steam\033[0m"
 					
 					sudo add-apt-repository multiverse
 					sudo apt update
 					sudo apt install steam
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 					
 					sleep 4
 					;;
 
                	# KeePass2
-				2) 	echo "Ahora empezará la descarga de KeePass2"
+				2) 	echo -e "\033[1;32mAhora empezará la descarga de KeePass2\033[0m"
 					
 					sudo apt install keepass2 -y
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 
 					sleep 4 
 
-					echo -e "Comprobando la versión de KeePass2"
+					echo -e "\033[1;32mComprobando la versión de KeePass2\033[0m"
 					sleep 2
 					keepass2 --version
 					
@@ -661,17 +661,17 @@ case $mainOption in
 					;;
 
                 # BitWarden
-				3) 	echo "Ahora empezará la descarga de BitWarden"
+				3) 	echo -e "\033[1;32mAhora empezará la descarga de BitWarden\033[0m"
 					
 					sudo snap install bitwarden
 					sleep 2
-					echo -e "Instalación completada"
+					echo -e "\033[1;32mInstalación completada\033[0m"
 					
 					sleep 4 
 					;;
 
 				# Exit
-				4) echo "Volviendo al menú principal"
+				4) echo -e "\033[1;32mVolviendo al menú principal\033[0m"
 					
 					sleep 2
 					break
@@ -698,19 +698,23 @@ case $mainOption in
 			echo -e "\033[1;36m1) Exit\033[0m"
 			echo -e "\033[1;36m========================\033[0m"
 
+			echo -e "\n "
+
 			echo -e "\033[1;36mOption Master nace de la necesidad de aprobar la asignatura de Sistemas Informáticos.\033[0m"
 			echo -e "\033[1;36mImpartida por el grandioso Marc Costa.\033[0m"
 
+			echo -e "\n "
+
 			echo -e "\033[1;36mAutores de la gran proeza:\033[0m"
 			echo -e "\033[1;36m=========================\033[0m"
-			echo -e "\033[1;36mDavid\033[0m"
-			echo -e "\033[1;36mSergio\033[0m"
-			echo -e "\033[1;36mAntonio Maroto\033[0m"
+			echo -e "\033[1;36m2)David Ramírez\033[0m"
+			echo -e "\033[1;36m3)Sergi Majada\033[0m"
+			echo -e "\033[1;36m4)Antonio Maroto\033[0m"
 			echo -e "\033[1;36m=========================\033[0m"
 
 			echo -e "\033[1;36mNo hay mucho más que ver aquí, vuelve por donde has venido.\033[0m"
 
-			read -p "Selecciona una opción: " nestedOption
+			read -p $'\033[1;32mSelecciona una opción:\033[0m ' nestedOption
 
 			if ! [[ $nestedOption =~ $validation ]]; then
 				echo -e "\033[1;31mPor favor, introduce un número.\033[0m"
@@ -721,11 +725,32 @@ case $mainOption in
 			case $nestedOption in
 
 				# Exit
-				1) echo "Volviendo al menú principal"
+				1) echo -e "\033[1;32mVolviendo al menú principal\033[0m"
 					
 					sleep 2
 					break
 					;;
+
+				2) echo -e "\033[1;32mAbriendo el perfil de GitHub de David Ramírez\033[0m"
+            
+		           sleep 2
+		           xdg-open "https://github.com/davidzz-code";
+		           sleep 2
+		           ;;
+
+		        3) echo -e "\033[1;32mAbriendo el perfil de GitHub de Sergi Majada\033[0m"
+            
+		           sleep 2
+		           xdg-open "https://github.com/smajada";		          
+		           sleep 2
+		           ;;
+
+				4) echo -e "\033[1;32mAbriendo el perfil de GitHub de Antonio Maroto\033[0m"
+            
+		           sleep 2
+		           xdg-open "https://github.com/AMaroto98";		          
+		           sleep 2
+		           ;;
 
 				# Resto de opciones
 				*) echo -e "\033[1;31mPor favor, introduce un número valido.\033[0m"
@@ -739,9 +764,9 @@ case $mainOption in
 		;;
 
 	# Exit
-	10) echo "Saliendo del programa"
+	10) echo -e "\033[1;32mSaliendo del programa\033[0m"
 
-		echo "Muchas gracias por utilizar Option Master."
+		echo -e "\033[1;32mMuchas gracias por utilizar Option Master.\033[0m"
 		
 		sleep 2
 
